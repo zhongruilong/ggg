854760707e5af65f0c8fcb44ab496f74ae1be19f
zhongruilong <389517349@qq.com>
2020-03-10 19:19:50 +0800

    第一次提交项目到本地仓库

---
+++ src/main/java/com/huhatutu/test/git/TestGitApplication.java
@@ -0,0 +1,13 @@
+package com.huhatutu.test.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+
+}
